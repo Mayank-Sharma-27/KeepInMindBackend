d604db60240778a42a2face9355278d4e54bdf9d
Mayank-Sharma-27 <mayank.sharma2796@gmail.com>
2024-08-28 06:40:34 -0700

    Add user handler

--- app/main.go
+++ app/main.go
@@ -1,2 +1,22 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/Mayank-Sharma-27/KeepInMindBackend.git/handlers"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New(fiber.Config{
+		AppName:      "KeepInMind",
+		ServerHeader: "Fiber",
+	})
+	server := app.Group("/api")
+
+	privateRoutes := server.Use()
+	handlers.NewUserHandler(privateRoutes.Group("/user"))
+	fmt.Print("server started")
+	app.Listen(fmt.Sprintf(":8080"))
+}
